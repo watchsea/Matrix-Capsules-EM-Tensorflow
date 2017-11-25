6f1a61f132a69e430163507249d3ba875412356c
watchsea <Iop[]\890>
2017-11-25 16:52:51 +0800

    according to my comprehension to create the model

--- eval.py
+++ eval.py
@@ -13,12 +13,16 @@ import os
 import capsnet_em as net
 
 def main(_):
+    coord_add = [[[8., 8.], [12., 8.], [16., 8.]],
+                 [[8., 12.], [12., 12.], [16., 12.]],
+                 [[8., 16.], [12., 16.], [16., 16.]]]
+
     with tf.Graph().as_default():
         num_batches_per_epoch_train = int(60000/cfg.batch_size)
         num_batches_test = int(10000/cfg.batch_size)
 
         batch_x, batch_labels = create_inputs(is_train=False)
-        output = net.build_arch(batch_x, is_train=False)
+        output = net.build_arch(batch_x, coord_add, is_train=False)
         batch_acc = net.test_accuracy(output, batch_labels)
         saver = tf.train.Saver()
 
@@ -36,12 +40,18 @@ def main(_):
                 ckpt = os.path.join(cfg.logdir, 'model.ckpt-%d' % (num_batches_per_epoch_train*epoch))
                 saver.restore(sess, ckpt)
 
+                accuracy_sum = 0
                 for i in range(num_batches_test):
-                    summary_str = sess.run(summary_op)
+                    batch_acc_v, summary_str = sess.run([batch_acc, summary_op])
                     print('%d batches are tested.' % step)
                     summary_writer.add_summary(summary_str, step)
 
+                    accuracy_sum += batch_acc_v
+
                     step += 1
+
+                ave_acc = accuracy_sum/num_batches_test
+                print('the average accuracy is %f' % ave_acc)
 
 if __name__ == "__main__":
     tf.app.run()
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ E-mail: zhangsuofei at njupt.edu.cn
 """
 
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
 from config import cfg
 from utils import create_inputs
 import time
@@ -13,7 +14,13 @@ import os
 import capsnet_em as net
 
 def main(_):
-    with tf.Graph().as_default():
+    coord_add = [[[8., 8.], [12., 8.], [16., 8.]],
+                 [[8., 12.], [12., 12.], [16., 12.]],
+                 [[8., 16.], [12., 16.], [16., 16.]]]
+
+    coord_add = np.array(coord_add, dtype=np.float32)/28.
+
+    with tf.Graph().as_default(), tf.device('/cpu:0'):
         global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
         num_batches_per_epoch = int(60000/cfg.batch_size)
@@ -22,15 +29,20 @@ def main(_):
         batch_x, batch_labels = create_inputs(is_train=True)
         # batch_y = tf.one_hot(batch_labels, depth=10, axis=1, dtype=tf.float32)
 
-        output = net.build_arch(batch_x, is_train=True)
+        m_op = tf.placeholder(dtype=tf.float32, shape=())
+        with tf.device('/gpu:0'):
+            with slim.arg_scope([slim.variable], device='/cpu:0'):
+                output = net.build_arch(batch_x, coord_add, is_train=True)
+                # loss = net.cross_ent_loss(output, batch_labels)
+                loss = net.spread_loss(output, batch_labels, m_op)
 
-        loss = net.cross_ent_loss(output, batch_labels)
-        loss_name = 'cross_ent_loss'
+            grad = opt.compute_gradients(loss)
+
+        loss_name = 'spread_loss'
 
         summaries = []
         summaries.append(tf.summary.scalar(loss_name, loss))
 
-        grad = opt.compute_gradients(loss)
         train_op = opt.apply_gradients(grad, global_step=global_step)
 
         sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
@@ -56,24 +68,30 @@ def main(_):
 
         summary_writer = tf.summary.FileWriter(cfg.logdir, graph=sess.graph)
 
-        for step in range(cfg.epoch*num_batches_per_epoch):
-            tic = time.time()
-            _, loss_value = sess.run([train_op, loss])
-            print('%d iteration is finished in ' % step + '%f second' % (time.time()-tic) + ',loss: %f'% loss_value)
-            # test1_v = sess.run(test2)
+        m = 0.2
+        for step in range(cfg.epoch):
+            for i in range(num_batches_per_epoch):
+                tic = time.time()
+                _, loss_value = sess.run([train_op, loss], feed_dict={m_op: m})
+                print('%d/%d, %d/%d iteration is finished in ' % (step,cfg.epoch,i,num_batches_per_epoch) + '%f second' % (time.time()-tic) + ',loss: %f'% loss_value)
+                # test1_v = sess.run(test2)
 
-            # if np.isnan(loss_value):
-            #     print('bbb')
-            #  assert not np.isnan(np.any(test2_v[0])), 'a is nan'
-            assert not np.isnan(loss_value), 'loss is nan'
+                # if np.isnan(loss_value):
+                #     print('bbb')
+                #  assert not np.isnan(np.any(test2_v[0])), 'a is nan'
+                assert not np.isnan(loss_value), 'loss is nan'
 
-            if step % 10 == 0:
-                summary_str = sess.run(summary_op)
-                summary_writer.add_summary(summary_str, initial_step+step)
+                if step % 10 == 0:
+                    summary_str = sess.run(summary_op, feed_dict={m_op: m})
+                    summary_writer.add_summary(summary_str, initial_step+step)
 
-            if (step % num_batches_per_epoch) == 0:
-                ckpt_path = os.path.join(cfg.logdir, 'model.ckpt')
-                saver.save(sess, ckpt_path, global_step=initial_step+step)
+            if step > 0:
+                m += (0.9-0.2)/(cfg.epoch*0.6)
+                if m > 0.9:
+                    m = 0.9
+
+            ckpt_path = os.path.join(cfg.logdir, 'model.ckpt')
+            saver.save(sess, ckpt_path, global_step=initial_step+step)
 
 if __name__ == "__main__":
     tf.app.run()
